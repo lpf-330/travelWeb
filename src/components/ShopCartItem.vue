8c370572bf028ace171e2db46f0b92ad8866874c
lpf-330 <Lpf7896325@outlook.com>
2025-03-16 13:11:43 +0800

    完成购物车前端样式

---
+++ src/components/ShopCartItem.vue
@@ -0,0 +1,151 @@
+<script setup>
+import { Delete } from '@element-plus/icons-vue'
+</script>
+
+<template>
+    <div class="item">
+        <div class="contain">
+            <div class="selectBox">
+                <input type="checkbox" name="" id="">
+            </div>
+            <div class="imgBox"></div>
+            <div class="nameBox">
+                <div class="name">
+                    <span>{{}}</span>
+                </div>
+                <div class="unitPrice">
+                    <span>单价：{{}} 元</span>
+                </div>
+            </div>
+            <div class="numBox">
+                <div class="num">
+                    <button class="button"> - </button>
+                    <div class="showNum">
+                        <span>{{ }}</span>
+                    </div>
+                    <button class="button"> + </button>
+                </div>
+                <el-button type="danger" :icon="Delete" circle />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.item {
+    height: 10rem;
+    width: 100%;
+    border-style: solid;
+    border-color: #ccc;
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    border-bottom-width: 0.1rem;
+    padding: 1%;
+    box-sizing: border-box;
+}
+
+.contain {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+}
+
+.selectBox {
+    height: 100%;
+    width: 10%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.imgBox {
+    height: 100%;
+    width: 20%;
+    padding: 1%;
+    box-sizing: border-box;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+}
+
+.nameBox {
+    height: 100%;
+    width: 50%;
+    box-sizing: border-box;
+    padding: 2%;
+    display: flex;
+    flex-direction: row;
+}
+
+.numBox {
+    height: 100%;
+    width: 20%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+}
+
+input[type="checkbox"] {
+    accent-color: aqua;
+    /* 勾选后的背景颜色 */
+    width: 1.5rem;
+    height: 1.5rem;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    cursor: pointer;
+}
+
+/* 如果想进一步自定义，可以增加以下样式 */
+input[type="checkbox"]:checked {
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    background-color: aqua;
+}
+
+
+.num {
+    height: 30%;
+    width: 60%;
+    background-color: cadetblue;
+    display: flex;
+    flex-direction: row;
+}
+
+.button {
+    width: 30%;
+    font-size: 1.8rem;
+    cursor: pointer;
+}
+
+.showNum {
+    width: 40%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.name {
+    height: 100%;
+    width: 70%;
+}
+
+.name {
+    font-size: 1.2rem;
+}
+
+.unitPrice {
+    height: 100%;
+    width: 30%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.unitPrice span {
+    font-size: 1rem;
+}
+</style>
--- src/view/shopCart.vue
+++ src/view/shopCart.vue
@@ -1,6 +1,16 @@
 <script setup>
+import { ElContainer } from 'element-plus';
+import ShopCartItem from '../components/ShopCartItem.vue';
+import { ref } from 'vue';
+
+const selected = ref([1, 2, 3, 4, 5, 6])
+
+const sumNum = ref(0)
+const sumPrice = ref(0)
+
 
 </script>
+
 
 <template>
     <div class="contain">
@@ -12,11 +22,55 @@
                 </el-header>
                 <el-main class="main">
                     <el-scrollbar max-height="100%" class="scrollbar">
-
+                        <shop-cart-item></shop-cart-item>
+                        <shop-cart-item></shop-cart-item>
+                        <shop-cart-item></shop-cart-item>
+                        <shop-cart-item></shop-cart-item>
+                        <shop-cart-item></shop-cart-item>
+                        <shop-cart-item></shop-cart-item>
+                        <shop-cart-item></shop-cart-item>
                     </el-scrollbar>
                 </el-main>
             </el-container>
-            <el-aside class="aside">Aside</el-aside>
+            <el-aside class="aside">
+                <div class="asideContain">
+                    <div class="view">
+                        <div class="viewHead">
+                            <div class="selectHeadImg">
+                                <span>商品</span>
+                            </div>
+                            <div class="selectHeadNum">
+                                <span>数量</span>
+                            </div>
+                            <div class="selectHeadPrice">
+                                <span>价格</span>
+                            </div>
+                        </div>
+                        <div class="scrollbarBox">
+                            <el-scrollbar max-height="100%" style="width: 100%;">
+                                <div class="selectItem" v-for="item in selected">
+                                    <div class="selectImg"></div>
+                                    <div class="selectNum">
+                                        <span></span>
+                                    </div>
+                                    <div class="selectPrice">
+                                        <span>{{ }} 元</span>
+                                    </div>
+                                </div>
+                            </el-scrollbar>
+                        </div>
+                        <div class="selectSum">
+                            <div class="sumBox">
+                                <div class="sumNum">已选择 {{ selected.length }} 件商品</div>
+                                <div class="sumPrice">总价：{{ sumPrice }} 元</div>
+                            </div>
+                            <div class="checkBox">
+                                <button class="checkButton">结算</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </el-aside>
         </el-container>
     </div>
 </template>
@@ -29,12 +83,6 @@
 
 .body {
     height: 100%;
-    width: 70%;
-}
-
-.aside {
-    height: 100%;
-    width: 30%;
 }
 
 .header {
@@ -53,10 +101,159 @@
 
 .main {
     padding-left: 10%;
+    padding-bottom: 4%;
 }
 
 .scrollbar {
     width: 100%;
-    background-color: beige;
+    background-color: #f1f1f1;
+    border-radius: 1rem;
+}
+
+.aside {
+    height: 100%;
+    width: 35%;
+    display: flex;
+    align-items: center;
+    justify-content: right;
+    padding-right: 6.5%;
+}
+
+.asideContain {
+    height: 60%;
+    width: 80%;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.37);
+    border-radius: 1rem;
+    box-sizing: border-box;
+    padding: 4%;
+}
+
+.view {
+    width: 100%;
+    height: 100%;
+}
+
+.selectSum {
+    height: 20%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+}
+
+.scrollbarBox {
+    height: 70%;
+    width: 100%;
+}
+
+.viewHead {
+    width: 100%;
+    height: 10%;
+    background-color: #f1f1f1;
+    display: flex;
+    flex-direction: row;
+}
+
+.sumBox {
+    height: 100%;
+    width: 60%;
+}
+
+.sumNum {
+    height: 50%;
+    width: 100%;
+    font-size: 1rem;
+    box-sizing: border-box;
+    padding-left: 15%;
+    display: flex;
+    align-items: center;
+}
+
+.sumPrice {
+    height: 50%;
+    width: 100%;
+    font-size: 1.2rem;
+    box-sizing: border-box;
+    padding-left: 15%;
+    display: flex;
+    align-items: center;
+    color: red;
+}
+
+.checkBox {
+    height: 100%;
+    width: 40%;
+    box-sizing: border-box;
+    padding: 6% 8% 6% 8%;
+}
+
+.checkButton {
+    height: 100%;
+    width: 100%;
+    background-color: red;
+    color: #fff;
+    font-size: 1.5rem;
+    cursor: pointer;
+    border-radius: 0.4rem;
+}
+
+.selectItem {
+    width: 100%;
+    height: 5rem;
+    margin-bottom: 4%;
+    display: flex;
+    flex-direction: row;
+}
+
+.selectHeadImg,
+.selectHeadNum,
+.selectHeadPrice,
+.selectImg,
+.selectNum,
+.selectPrice {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.selectHeadImg {
+    width: 30%;
+}
+
+.selectHeadNum {
+    width: 35%;
+}
+
+.selectHeadPrice {
+    width: 35%;
+}
+
+.selectImg,
+.selectNum,
+.selectPrice {
+    height: 5rem;
+    border-style: solid;
+    border-color: #dfdfdf;
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    border-bottom-width: 0.2rem;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.selectImg {
+    width: 30%;
+    padding: 2%;
+}
+
+.selectNum {
+    width: 35%;
+}
+
+.selectPrice {
+    width: 35%;
 }
 </style>
